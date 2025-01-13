79c93dcf376535cf23a140a3df0a1ce4d30f3d84
Paul Mutawe <paulmutawe3@gmail.com>
2025-01-13 23:07:13 +0000

    Add SemanticSearchEngine.py

---
+++ SemanticSearchEngine.py
@@ -0,0 +1,33 @@
+from datasets import load_dataset
+from sentence_transformers import SentenceTransformer, util
+import torch
+
+dataset = load_dataset("multi_news", split="test")
+
+df = dataset.to_pandas().sample(2000, random_state = 10)
+
+model = SentenceTransformer("all-MiniLM-L6-v2")
+
+passage_embeddings = list(model.encode(df['summary'].to_list(), show_progress_bar = True))
+
+passage_embeddings[0].shape
+
+query = "Find me some articles about technology and artificial intelligence"
+
+def find_relevant_news(query):
+
+    query_embedding = model.encode(query)
+
+    query_embedding.shape
+
+    similarities = util.cos_sim(query_embedding, passage_embeddings)
+
+    top_indices = torch.topk(similarities.flatten(), 3).indices
+
+    top_relevant_passages = [df.iloc[x.item()]['summary'][:200] + "..." for x in top_indices]
+
+    return top_relevant_passages
+
+find_relevant_news("Natural Disasters")
+find_relevant_news("Police")
+find_relevant_news("Weather forecast")
